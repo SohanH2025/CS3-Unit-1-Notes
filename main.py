12b474860748e6f356c9ab2fd71db8ab951202e9
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-09-12 18:25:12 +0000

    removed code

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 def main():
-    #print("hello world")
-
-    xpoints = np.array([0, 10])
-    ypoints = np.array([0, 10])
-
-    plt.plot(xpoints, ypoints)
-    plt.show()
-
-    #declare hotdigs
-    #hotdog = "beef"
-    #burger = "smashed"
-    #num = 2
-    #math(x=2,y=3,z=4)
-    #got = type(num)
-    #print(got)
-
-    #print(f"my favorite hotdog is {hotdog} nhh bunhgfr {hotdog} jssjjs {2+2}")
-
+    
 def math(x,y,z):
     print(x+y+z)
 
