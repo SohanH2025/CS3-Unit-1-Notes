cac961787fea48810612e28e09523ffa3bb9094f
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-09-13 14:35:31 +0000

    changed project direction

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 
 
 def main():
+    print("this is a bot that helps solve NYT Connections by showing all posible solutions left when at 2/4 connections solved\n    when two connections are left there are only two possibilities in a guess: you either get one off or nothing. \n        one off is self expainitory, one of the words is inncorrect. \n        nothing means, when they're only two groups left, that your guess has two words from one group and two words from the other. \n.   another key idea is that each guess will have the same result (ie one away or nothing) with its opposite guess meaning that if your guess is one away the opposite guess available ")
+    print("assume the top left is numbered 1 and to its right is 2, 3, 4, then the bottom left is 5 and to its right is 6, 7, 8")
+
+
+    num = input("what numbers were your guess? ")
+    inp = input("input: was your guess one away (answer with a 1) or did nothing pop up (answer with a 2)")
     
-def math(x,y,z):
-    print(x+y+z)
+    
+def fibbonachi(digits):
+    for i in range(digits):
+        break
+
+
+    
 
 
 
